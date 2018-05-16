b6e1956f909f1b76a0747e4965fa54a64d8cb590
wwendyc <wendy.changw@gmail.com>
2018-05-16 08:24:40 -0400

    deployment update

--- server/middleware/body-parsing.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const bodyParser = require('body-parser')
-module.exports = router
-
-// Body parsing middleware
-router.use(bodyParser.json())
-router.use(bodyParser.urlencoded({extended: true}))
--- server/middleware/index.js
+++ server/middleware/index.js
@@ -2,5 +2,4 @@ const router = require('express').Router()
 module.exports = router
 
 router.use(require('./logging'))
-router.use(require('./body-parsing'))
 router.use(require('./static'))
